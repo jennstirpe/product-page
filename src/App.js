f0d18912e6636f4e9ee5886fbe94835c1b41c9b8
jstirpe <jennstirpe@yahoo.com>
2022-08-22 17:01:50 -0400

    Fixed issue with local storage not persisting

--- src/App.js
+++ src/App.js
@@ -11,31 +11,34 @@ import Header from "./components/Header";
 import Product from "./components/Product";
 
 
-
+const LOCAL_STORAGE_KEY = "ecommerceSite.cart";
 
 function App() {
-  const [ cartContents, setCartContents ] = useState([ 
-    {
-      id: "testId",
-      name: "testName",
-      price: 100,
-      quantity: 2,
-      img: testImg
-    },
-    {
-      id: "testId2",
-      name: "testName2 test testName testNaming",
-      price: 150,
-      quantity: 1,
-      img: testImg
-    }  
-  ]);
+  // const [ cartContents, setCartContents ] = useState([ 
+  //   {
+  //     id: "testId",
+  //     name: "testName",
+  //     price: 100,
+  //     quantity: 2,
+  //     img: testImg
+  //   },
+  //   {
+  //     id: "testId2",
+  //     name: "testName2 test testName testNaming",
+  //     price: 150,
+  //     quantity: 1,
+  //     img: testImg
+  //   }  
+  // ]);
 
-
+  const [ cartContents, setCartContents ] = useState(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []);
   const [ itemCount, setItemCount ] = useState(0);
 
   useEffect(() => {
-    getItemCount()
+    if(cartContents.length !== 0) {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(cartContents));
+    }
+    getItemCount();
   }, [cartContents])
 
   function getItemCount() {
@@ -53,8 +56,8 @@ function App() {
     setItemCount(sum)
   } 
 
-  function addToCart(name, price, quantity, img) {
 
+  function addToCart(name, price, quantity, img) {
     // check if item already in cart, if so, update amount
     const existingCartItem = cartContents.find(item => item.id == name);
 
@@ -67,12 +70,12 @@ function App() {
           return item;
         }
       })
-
       setCartContents(updatedCart);
     } else { // if item not already in cart, add as new item
       setCartContents([...cartContents, { id: name, name: name, price: price, quantity: quantity, img: img }]);
     }
   }
+
 
   return (
     <ThemeProvider theme={lightTheme}>
