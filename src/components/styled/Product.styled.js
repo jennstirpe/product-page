02ce7957039edbd250460554f5ba09dd82d47d08
jstirpe <jennstirpe@yahoo.com>
2022-08-25 14:50:44 -0400

    Updated product styling

--- src/components/styled/Product.styled.js
+++ src/components/styled/Product.styled.js
@@ -14,6 +14,7 @@ export const StyledProduct = styled.main`
         }
 
         .gallery-btn {
+            pointer-events: auto;
             position: absolute;
             top: 50%;
             height: 2.5rem;
